7095a9791fa16f2095c0b054f6a083704d31b243
Mrigank Tandon <sachitkapoor01@gmail.com>
2020-06-10 20:45:47 +0530

    initial commit

---
+++ app/src/main/java/com/mrigank/PocketLibrary/ItemClickListener.java
@@ -0,0 +1,7 @@
+package com.mrigank.PocketLibrary;
+
+import android.view.View;
+
+public interface ItemClickListener {
+    void onClick(View view, int position, boolean isLongClick);
+}
---
+++ app/src/main/java/com/mrigank/PocketLibrary/PDFAdapter.java
@@ -0,0 +1,63 @@
+package com.mrigank.PocketLibrary;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class PDFAdapter extends RecyclerView.Adapter<PDFAdapter.Holder> {
+
+    private List<PDFModel> list;
+    private Context context;
+    ItemClickListener itemClickListener;
+
+    public PDFAdapter(List<PDFModel> list, Context context, ItemClickListener itemClickListener) {
+        this.list = list;
+        this.context = context;
+        this.itemClickListener = itemClickListener;
+    }
+
+    @NonNull
+    @Override
+    public Holder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(context).inflate(R.layout.pdf_item,parent,false);
+        Holder holder = new Holder(v);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Holder holder, int position) {
+
+        holder.pdfName.setText(list.get(position).getPdfName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class Holder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        private TextView pdfName;
+        private ImageView imageView;
+
+        public Holder(View itemView) {
+            super(itemView);
+            pdfName = itemView.findViewById(R.id.TV);
+            imageView = itemView.findViewById(R.id.IV);
+
+            imageView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            itemClickListener.onClick(v,getAdapterPosition(),false);
+        }
+    }
+}
